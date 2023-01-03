1106f32fdcf71404739c730479d7bf6ddc22bf0f
@yurisa2 <yuri@sa2.com.br>
2023-01-03 16:13:15 -0300

    GP

--- app/petrosa_backtesting.py
+++ app/petrosa_backtesting.py
@@ -1,48 +1,93 @@
-import os
+from urllib.request import Request, urlopen
+import datetime
 import json
-import datetime
+import logging
 import time
-import logging 
-import numpy as np
-import pymongo
-from backtesting import Strategy, Backtest
-from app import get_data
-from backtesting.lib import plot_heatmaps
+
 import newrelic.agent
-import datetime
-import random
-from app import constants
+import numpy as np
+from backtesting import Backtest, Strategy
+
+from app import datacon
+
+import aiohttp
 import requests
+import json
+
+
+def request_setup(post_data):
+    from urllib.parse import urlencode
+
+
+    # inside_bar_value = requests.post(
+    #     # f"http://localhost:8090/inside_bar_buy/{self.timeframe}",
+    #     f"http://localhost:8090/inside_bar_buy/m15",
+    #     data=post_data)
+    
+    url = 'http://localhost:8090/inside_bar_buy/m15'  # Set destination URL here
+
+    data = post_data     # Set POST fields here
+    # data = json.dumps(post_data)     # Set POST fields here
+    req = Request(url, urlencode(data).encode())
+    inside_bar_value = urlopen(req).read().decode()
+
+    print(inside_bar_value)
+
+    return inside_bar_value
 
 class bb_backtest(Strategy):
-    buy_sl=None
+    buy_sl = None
+    buy_tp = None
+    sell_sl = None
+    sell_tp = None
+    buy_threshold = None
+    sell_threshold = None
+    sell_enabled = True
+    buy_enabled = True
+
     def init(self) -> None:
         pass
-
 
     @newrelic.agent.background_task()
     def next(self):
 
+        if (
+            self.buy_sl is None
+            or self.buy_tp is None
+            or self.sell_sl is None
+            or self.sell_tp is None
+            or self.buy_threshold is None
+            or self.sell_threshold is None
+            or self.sell_enabled is None
+            or self.buy_enabled is None
+        ):
+            return True
+
+        if (self.buy_sl > self.buy_tp
+                or self.sell_sl > self.sell_tp):  # should be done via constraints
+            return True
+
         try:
-            if(self.data.index[-1] in self.main_data.index):
+            if (self.data.index[-1] in self.main_data.index):
                 work_data = self.main_data.loc[self.main_data.index
                                                <= self.data.index[-1]]
-                # print('work_data', work_data.index[-1])
-                # print('main_Data', self.data.index[-1
 
-                # print(self.data.index[-1])
-                
                 post_data = work_data.sort_values(
                     by='datetime', ascending=False)
                 post_data = post_data[:201]
-                
-                inside_bar_value = requests.post(
-                    f"http://localhost:8090/inside_bar_buy/{self.timeframe}", 
-                    data=post_data)
-                
+
+                print('oi Im before')
+
+                # async with aiohttp.ClientSession() as session:
+                #     async with session.get('http://httpbin.org/get') as resp:
+                #         print(resp.status)
+                #         print(await resp.text())
+
+                # print(request_setup(post_data))
+
                 print("oi " * 100)
                 print("im inside")
-                print(inside_bar_value)
+
 
             else:
                 return True
@@ -52,111 +97,67 @@ class bb_backtest(Strategy):
 
 
 @newrelic.agent.background_task()
-def run_backtest(symbol, test_period, strategy_name):
+def run_backtest(symbol, test_period):
 
-    data = get_data.get_data(symbol, 'm5', limit=2000)
-    main_data = get_data.get_data(symbol, test_period, limit=220)
+    data = datacon.get_data(symbol, 'm5', limit=1000)
+    main_data = datacon.get_data(symbol, test_period, limit=200)
 
-    if(len(data) == 0 or len(main_data) == 0):
+    if (len(data) == 0 or len(main_data) == 0):
         return False
 
     strat = bb_backtest
     strat.main_data = main_data
-    
-    strat.timeframe = test_period
 
     bt = Backtest(
-                    data,
-                    strat,
-                    commission=0,
-                    exclusive_orders=True,
-                    cash=100000)
+        data,
+        strat,
+        commission=0,
+        exclusive_orders=True,
+        cash=100000)
 
-    stats, heatmap = bt.optimize(
-        buy_sl=[1],
-
+    stats = bt.optimize(
+        buy_sl=list(np.arange(1, 3, 0.5)),
+        buy_tp=list(np.arange(1, 4, 0.5)),
+        sell_sl=list(np.arange(1, 3, 0.5)),
+        sell_tp=list(np.arange(1, 4, 0.5)),
+        buy_threshold=list(np.arange(1, 3, 0.5)),
+        sell_threshold=list(np.arange(1, 3, 0.5)),
         maximize='SQN',
         # minimize='Max. Drawdown [%]',
         max_tries=200,
         random_state=0,
-        return_heatmap=True)
-    # plot_heatmaps(heatmap, agg='mean')
+        return_heatmap=False)
 
-    client = pymongo.MongoClient(
-                os.getenv(
-                    'MONGO_URI', 'mongodb://root:QnjfRW7nl6@localhost:27017'),
-                readPreference='secondaryPreferred',
-                appname='petrosa-strategy-screenings-backtesting'
-                                        )
-
-    heatmap = heatmap.dropna().sort_values().iloc[-10:]
     new_hm = {}
-    new_hm['heatmap'] = heatmap
     new_hm['insert_timestamp'] = datetime.datetime.now()
-    new_hm['strategy'] = strategy_name
+    new_hm['strategy'] = 'simple_gap_finder'
     new_hm['period'] = test_period
     new_hm['symbol'] = symbol
 
     doc = json.dumps({**stats._strategy._params,
-                     ** stats, **new_hm}, default=str)
+                     **stats, **new_hm}, default=str)
     doc = json.loads(doc)
 
-    client.petrosa_crypto['backtest_results'].update_one(
-                                            {"strategy": strategy_name,
-                                             "symbol": symbol,
-                                             "period": test_period
-                                             }, {"$set": doc}, upsert=True)
+    datacon.post_results(symbol, test_period, doc)
 
 
 @newrelic.agent.background_task()
-def continuous_run() -> None:
-    client = pymongo.MongoClient(
-                os.getenv(
-                    'MONGO_URI', 'mongodb://root:QnjfRW7nl6@localhost:27017'),
-                readPreference='secondaryPreferred',
-                appname='petrosa-strategy-screenings-backtesting'
-                                        )
+def continuous_run():
     try:
-        params = client.petrosa_crypto['backtest_controller'].find(
-            {"status": 0, "strategy": {"$in": constants.screenings}})
-        params = list(params)
+        params = datacon.find_params()
 
-        if len(params) == 0:
-            params = client.petrosa_crypto['backtest_controller'].find(
-                {"status": 1, "strategy": {"$in": constants.screenings}})
-            params = list(params)
-
- 
-        if len(params) == 0:
-            params = client.petrosa_crypto['backtest_controller'].find(
-                {"strategy": {"$in": constants.screenings}})
-            params = list(params)
-        
-        # params = params[random.randint(0, len(params))]
-        params = params[0]
-
-
-
-        client.petrosa_crypto['backtest_controller'].update_one(
-            params, {"$set": {"status": 1}})
-
-        logging.warning('Running backtest for screenings on: ' + 
+        logging.warning('Running backtest for simple_gap_finder on: ' +
                         str(params))
-        bt_ret = run_backtest(params['symbol'], params['period'], params['strategy'])
+        bt_ret = run_backtest(params['symbol'], params['period'])
 
         if bt_ret is False:
-            status = -1
+            datacon.update_status(params=params, status=-1)
         else:
-            status = 2
-
-        client.petrosa_crypto['backtest_controller'].update_one(
-            {"_id": params['_id']}, {"$set": {"status": status}})
-
+            datacon.update_status(params=params, status=2)
         logging.warning('Finished ' + str(params))
 
-        pass
     except Exception as e:
         logging.error(e)
         time.sleep(10)
 
-# bt.run()
+    return True
